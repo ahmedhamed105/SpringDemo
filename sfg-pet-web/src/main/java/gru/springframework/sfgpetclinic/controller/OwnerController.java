4587c489845fd80034d95e1da6f0c6209ac2243d
Ahmed Elemam <ahmed.elemam@its.ws>
2019-08-26 18:23:27 +0200

    adding owners list find all

--- sfg-pet-web/src/main/java/gru/springframework/sfgpetclinic/controller/OwnerController.java
+++ sfg-pet-web/src/main/java/gru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,14 +1,30 @@
 package gru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import gru.springframework.sfgpetclinic.services.OwnerService;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 	
+	private final OwnerService ownerservice;
 	
+	
+	
+	
+	public OwnerController(OwnerService ownerservice) {
+		this.ownerservice = ownerservice;
+	}
+
+
+
+
 	@RequestMapping({"","/","/index","/index.html"})
-	public String ListVets() {
+	public String ListVets(Model model) {
+		
+		model.addAttribute("owners",ownerservice.findAll());
 		
 		return "owners/index";
 	}
